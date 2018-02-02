45cc8f6e58a5aa8e5856ad528fbd9859414a4478
kingmarki <36058237+kingmarki@users.noreply.github.com>
2018-02-01 20:12:35 -0500

    Add files via upload
    Hello World assignment

---
+++ helloWorld.java
@@ -0,0 +1,17 @@
+
+public class helloWorld {
+	
+	private static String helloWorld() 
+	{
+		return "Hello World from Mark Lemus";
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println(helloWorld());
+
+	}
+
+
+
+}
