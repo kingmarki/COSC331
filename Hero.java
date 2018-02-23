5aa17262ed9b36d5dba4fdd9158e8f82bc1a6a9d
kingmarki <36058237+kingmarki@users.noreply.github.com>
2018-02-23 16:24:06 -0500

    Update Hero.java
    Hero class now extends Actor

--- Hero.java
+++ Hero.java
@@ -1,8 +1,26 @@
 
-public class Hero //extends Actor 
+public class Hero extends Actor 
 {
+	private int location;
+	
 	public String toString() 
 	{
 		return "H";
+	}
+	
+	public Hero(int location)
+	{
+		this.location = location;
+		type = "Hero";
+	}
+	
+	public int getLocation()
+	{
+		return location;
+	}
+	
+	public char print()
+	{
+		return 'H';
 	}
 }
