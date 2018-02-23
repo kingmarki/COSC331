872ea41bff78396d130c15b4fa02241883bd2021
kingmarki <36058237+kingmarki@users.noreply.github.com>
2018-02-23 16:24:51 -0500

    Update Actor.java
    Added content to actor class

--- Actor.java
+++ Actor.java
@@ -1,3 +1,19 @@
-public class Actor {
-
+public class Actor 
+{
+	public String type;
+	public int location;
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public char print()
+	{
+		return '_';
+	}
+	
+	public Actor(){
+		type = "blank";
+	}
 }
